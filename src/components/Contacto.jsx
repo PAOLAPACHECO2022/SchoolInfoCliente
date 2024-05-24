2501c82ca523e03ac71058d7b70943c22242e0b7
PAOLAPACHECO2022 <105870132+PAOLAPACHECO2022@users.noreply.github.com>
2024-05-24 18:14:24 -0500

    Update Contacto.jsx

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from "react-router-dom";
 export default class Contacto extends Component {
     
   render() {
-    
+    const navigate = useNavigate();
     return (
       
         <header class="fondoy fondoy-wrap p-5 d-flex">
