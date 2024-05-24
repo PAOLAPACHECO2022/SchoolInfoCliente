d0a7832b266c376d5eee9755611f7ef8494c454d
PAOLAPACHECO2022 <105870132+PAOLAPACHECO2022@users.noreply.github.com>
2024-05-24 13:56:21 -0500

    Update LoginPage.jsx

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -184,7 +184,7 @@ const LoginPage = () => {
                 </p>
               </strong>
             </div>
-            <a href= "https://schoolinfocliente.onrender.com/contactologin">
+            <a href= "https://schoolinfoserver.onrender.com/contactologin">
               <div
                 className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-500 dark:hover:bg-gray-500"
                 onClick={() => navigate("/contactologin")}
