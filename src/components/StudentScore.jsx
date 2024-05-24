a1ef0b8605e9fc2f13ce79a69c997c886b67a8eb
PAOLAPACHECO2022 <105870132+PAOLAPACHECO2022@users.noreply.github.com>
2024-05-24 17:05:39 -0500

    Update StudentScore.jsx

--- src/components/StudentScore.jsx
+++ src/components/StudentScore.jsx
@@ -14,10 +14,10 @@ const StudentScore = () => {
   const user = useSelector((state) => state.user);
   const [currentPage, setCurrentPage] = useState(0);
   const [searchTerm, setSearchTerm] = useState(""); // Estado para el término de búsqueda
-  const API_URL = process.env.REACT_APP_API_URL; 
+  
   const getScoresRecord = async () => {
     const response = await fetch(
-      `${API_URL}/scores/${user._id}/student`,
+      `https://schoolinfoserver.onrender.com/scores/${user._id}/student`,
       {
         method: "GET",
         headers: { Authorization: `Bearer ${token}` },
