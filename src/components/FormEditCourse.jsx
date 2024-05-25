92737638748013619c892babde37da0aed8cbe92
PAOLAPACHECO2022 <105870132+PAOLAPACHECO2022@users.noreply.github.com>
2024-05-25 07:42:16 -0500

    Update FormEditCourse.jsx

--- src/components/FormEditCourse.jsx
+++ src/components/FormEditCourse.jsx
@@ -36,9 +36,9 @@ const FormEditCourse = ({courseData, setCourseData, gradeId}) => {
   };
 
   return (
-    <div className="fondoy fondoy-wrap p-5">
+    <div className="fondoy fondoy-wrap p-5 overflow-y-auto overflow-x-auto">
     <div className="p-4 sm:ml-64">
-        <div className="fondox p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
+        <div className="fondox p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14 overflow-y-auto overflow-x-auto">
           <div className="flex flex-row justify-between mb-4">
             <h1 className="text-3xl font-bold mb-4">Edit course</h1>
           </div>
