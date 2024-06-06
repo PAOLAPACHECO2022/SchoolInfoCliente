327ce4fb92334862281d7a3cc7da44e1a8ae05bd
PAOLAPACHECO2022 <105870132+PAOLAPACHECO2022@users.noreply.github.com>
2024-06-06 12:57:22 -0500

    Update StudentActivity.jsx

--- src/components/StudentActivity.jsx
+++ src/components/StudentActivity.jsx
@@ -13,7 +13,6 @@ const StudentActivity = () => {
   const [currentPage, setCurrentPage] = useState(0);
   const [searchTerm, setSearchTerm] = useState(""); // Estado para el término de búsqueda
 
- 
   const getActivitiesRecord = async () => {
     const response = await fetch(
       `https://schoolinfoserver.onrender.com/activities/${user._id}/student`,
@@ -53,7 +52,7 @@ const StudentActivity = () => {
   const activitiesToDisplay = filteredActivities
     .slice(offset, offset + PER_PAGE)
     .map((activity) => (
-      <tr key={activity._id} >
+      <tr key={activity._id} className="bg-white hover:bg-gray-100 border-b">
         <td className="border  px-4 py-2">{activity.courseName}</td>
         <td className="border px-4 py-2">{activity.studentName}</td>
         <td className="border  px-4 py-2">{activity.area}</td>
@@ -68,41 +67,43 @@ const StudentActivity = () => {
 
   return (
     <>
-      <div className="p-4 sm:ml-64">
-        <div className="fondoy fondoy-wrap p-5 overflow-x-auto overflow-y-auto">
+     
+     <div className="p-4 sm:ml-64"> 
+        <div className="fondoy fondoy-wrap p-5">
           <Sidebar />
           <Aside />
           <div className="p-4 sm:ml-64">
-            <div className="bg-white p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14 overflow-x-auto overflow-y-auto">
-              <div className=" flex-row justify-between mb-4">
-                <h1 className="text-3xl font-bold mb-4">Activities</h1>
+            <div className="bg-white p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
+             <h1 className="text-3xl font-bold mb-4 tracking-wider">Activities</h1>
+            <div className="flex flex-col sm:flex-row mb-4">
+               
                 <input
                   type="text"
                   placeholder="Search by Area..."
                   value={searchTerm} // Se vincula al estado
                   onChange={(e) => setSearchTerm(e.target.value)} // Actualiza el estado
                   className="border px-4 py-2 rounded border-black"
-                  style={{ marginRight: '-2cm' }}
                 />
               </div>
               
-
+              <div className="overflow-x-auto overflow-y-auto">
               <table className=" table-auto w-full text-left bg-danger">
                 <thead>
                   <tr className="d-flex table-auto w-full text-left bg-danger">
-                    <th className="px-4 py-2">Course</th>
-                    <th className="px-4 py-2">Student</th>
-                    <th className="px-4 py-2">Area</th>
-                    <th className="px-4 py-2">Period</th>
-                    <th className="px-4 py-2">Indicator</th>
-                    <th className="px-4 py-2">Activity</th>
-                    <th className="px-4 py-2">State</th>
-                    <th className="px-4 py-2">Date</th>
+                    <th className="px-4 py-2 uppercase tracking-wider">Course</th>
+                    <th className="px-4 py-2 uppercase tracking-wider">Student</th>
+                    <th className="px-4 py-2 uppercase tracking-wider">Area</th>
+                    <th className="px-4 py-2 uppercase tracking-wider">Period</th>
+                    <th className="px-4 py-2 uppercase tracking-wider">Indicator</th>
+                    <th className="px-4 py-2 uppercase tracking-wider">Activity</th>
+                    <th className="px-4 py-2 uppercase tracking-wider">State</th>
+                    <th className="px-4 py-2 uppercase tracking-wider">Date</th>
                    
                   </tr>
                 </thead>
                 <tbody>{activitiesToDisplay}</tbody>
               </table>
+              </div>
               <div className="flex justify-center mt-4">
                 <ReactPaginate
                   previousLabel={"← Previous"}
