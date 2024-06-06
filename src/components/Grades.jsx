5468c70396e84bf63bc3394a7b44327f4382e191
PAOLAPACHECO2022 <105870132+PAOLAPACHECO2022@users.noreply.github.com>
2024-06-06 13:04:24 -0500

    Update Grades.jsx

--- src/components/Grades.jsx
+++ src/components/Grades.jsx
@@ -75,7 +75,7 @@ const Grades = () => {
   return (
     <>
       <div className="p-4 sm:ml-64"> 
-        <div className="fondoy fondoy-wrap p-5">
+        <div className="fondoy fondoy-wrap p-5 overflow-x-auto overflow-y-auto">
           <Sidebar />
           <Aside />
           <div className="p-4 sm:ml-64">
