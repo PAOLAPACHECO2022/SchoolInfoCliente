a47326fb986625ceacde8971c2440d307421a1ad
PAOLAPACHECO2022 <105870132+PAOLAPACHECO2022@users.noreply.github.com>
2024-06-06 13:02:42 -0500

    Update StudentActivity.jsx

--- src/components/StudentActivity.jsx
+++ src/components/StudentActivity.jsx
@@ -69,7 +69,7 @@ const StudentActivity = () => {
     <>
      
      <div className="p-4 sm:ml-64"> 
-        <div className="fondoy fondoy-wrap p-5">
+        <div className="fondoy fondoy-wrap p-5 overflow-x-auto overflow-y-auto">
           <Sidebar />
           <Aside />
           <div className="p-4 sm:ml-64">
