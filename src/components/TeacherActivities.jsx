b9ea8507323a11aa3df51753671a9020fc38c045
PAOLAPACHECO2022 <105870132+PAOLAPACHECO2022@users.noreply.github.com>
2024-06-06 22:56:03 -0500

    Update TeacherActivities.jsx

--- src/components/TeacherActivities.jsx
+++ src/components/TeacherActivities.jsx
@@ -1,21 +1,19 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
 import { setCourses } from "state";
 import Aside from "./Aside";
 import Sidebar from "./Sidebar";
 import CardCourseA from "./CardCourseA";
 import '../index.css'; 
-const CoursesByGradeActivity = () => {
+
+const TeacherActivities = () => {
     const dispatch = useDispatch();
     const courses = useSelector((state) => state.courses);
     const token = useSelector((state) => state.token);
-    const {gradeId} = useParams();
-
+    const user = useSelector((state) => state.user);
 
     const getCourses = async () => {
-        const response = await fetch(
-          `https://schoolinfoserver.onrender.com/courses/${gradeId}`, {
+        const response = await fetch(`https://schoolinfoserver.onrender.com/courses/${user._id}/teacher`, {
             method: "GET",
             headers: {Authorization: `Bearer ${token}`},
         });
@@ -25,16 +23,16 @@ const CoursesByGradeActivity = () => {
 
     useEffect(() => {
         getCourses();
-    }, [gradeId]); // eslint-disable-line
+    }, [user]); // eslint-disable-line
 
     return (
         <>
-           <div className="p-4 sm:ml-64"> 
-    <div className="fondoy fondoy-wrap p-5 overflow-x-auto overflow-y-auto">
+    <div className="p-4 sm:ml-64"> 
+    <div className="fondoy fondoy-wrap p-5 overflow-x-auto overflow-y-aut">
           <Sidebar />
           <Aside />
           <div className="p-4 sm:ml-64">
-            <div className=" p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14 overflow-x-auto overflow-y-auto">
+            <div className=" p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
               <div>
                 {courses.map((course) => (
                     <CardCourseA key={course._id} courseId={course._id} courseName={course.nameCourse} teacherName={course.teacherName} gradeId={course.gradeId} />
@@ -48,4 +46,4 @@ const CoursesByGradeActivity = () => {
       );
 };
 
-export default CoursesByGradeActivity;
+export default TeacherActivities;
